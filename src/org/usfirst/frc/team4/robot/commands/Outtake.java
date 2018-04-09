44a33e374b189e6f922c7ad8ef1d0f1ac3ad2c36
unknown <matthew.fulde@ht-la.org>
2018-04-09 10:39:42 -0700

    Added auto mode for far left switch

---
+++ src/org/usfirst/frc/team4/robot/commands/Outtake.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team4.robot.commands;
+
+import org.usfirst.frc.team4.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class Outtake extends Command {
+
+    public Outtake() {
+    	requires(Robot.m_intake);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.m_intake.setMotorSpeed(-.75);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
